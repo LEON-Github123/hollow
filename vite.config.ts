b93d4dbefe3fd16141bb4be088fb26949c5ec449
LEON-Github123 <LEON-Github123@users.noreply.github.com>
2026-04-17 10:46:17 +0800

    fix: 调整 Vite 打包大小警告限制至 2000kb

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // 修改打包体积警告阈值
+  },
   server: {
     proxy: {
       '/api-proxy': {
